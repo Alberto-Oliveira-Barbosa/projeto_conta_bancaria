3bd46516a63908878a1138203dc710fd9bfa67d4
Alberto Oliveira Barbosa <albertobarbosa115@gmail.com>
2025-02-11 08:25:00 -0300

    refactor: add functions

--- conta.py
+++ conta.py
@@ -1,50 +1,84 @@
-menu = """
+def get_menu():
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    menu = """
 
-=> """
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+    => """
 
-while True:
-    opcao = input(menu)
-    if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
-        excedeu_saldo = valor > saldo
-        excedeu_limite = valor > limite
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-    elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
-    elif opcao == "q":
-        break
+    return menu
+
+# o exercício pede que todos os argumentos 
+# passados para a função depositar sejam posicionais 
+def depositar(valor, saldo, extrato, / ):
+    if valor > 0:
+        saldo += valor
+        extrato += f"(+) Depósito: R$ {valor:.2f}\n"
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Operação falhou! O valor informado é inválido.")
+
+    return saldo, extrato
+
+# o exercício pede que todos os argumentos passados
+# sejam passados nomeados
+def sacar(*, valor, saldo, extrato, limite, numero_saques, LIMITE_SAQUES):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= LIMITE_SAQUES
+    
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"(-) Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+    return saldo, extrato, numero_saques
+
+# o exercício pede que os o seguinte para a função extrato:
+# - saldo como argumento posicional
+# - extrato como argumento nomeado
+def get_extrato(saldo, /, *, extrato ):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+        opcao = input(get_menu())
+        
+        if opcao.lower() == "d":
+            valor = float(input("Informe o valor do depósito: "))
+            saldo, extrato = depositar(valor, saldo, extrato)
+
+        elif opcao.lower() == "s":
+            valor = float(input("Informe o valor do saque: "))
+            saldo, extrato, numero_saques = sacar(valor=valor, saldo=saldo, extrato=extrato, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
+
+        elif opcao.lower() == "e":
+            get_extrato(saldo, extrato=extrato)
+
+        elif opcao.lower() == "q":
+            break
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+if __name__ == '__main__':
+    main()
